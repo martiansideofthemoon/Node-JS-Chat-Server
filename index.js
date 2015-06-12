bc1eb5fcaf16e520262d5fe6f16df2c4792ae78a
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-06-12 18:33:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var usernames=[];
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user has connected');
+
+  socket.on('disconnect', function(){
+    io.emit('chat message',usernames[socket.id]+' has disconnected');
+    console.log('a user has disconnected.');
+
+  });
+});
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('is typing', function(msg){
+    io.emit('is typing',usernames[socket.id] + msg);
+  });
+  socket.on('add user', function(msg){
+  	usernames[socket.id]=msg;
+    io.emit('chat message', usernames[socket.id]+' has connected.');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
